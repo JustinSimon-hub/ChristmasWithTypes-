6bb870e52d6dd55d8adbd893d920608dbbbbd3a8
Justin Simon <justinsimon@Justins-MBP.lan>
2022-12-31 18:38:06 -0800

    Fixed issue 2

---
+++ Day.cs
@@ -0,0 +1,8 @@
+﻿namespace ChristmasWithTypes
+{
+    public partial class Christmas
+    {
+        //TODO Make the property, "Day", type enum
+        public enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday/*Fill out the days of the week*/ };
+    }
+}
