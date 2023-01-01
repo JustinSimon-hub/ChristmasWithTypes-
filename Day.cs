01a6c1f8d6b75f2f623ca6be06453b1c9d78683f
Justin Simon <justinsimon@Justins-MBP.lan>
2022-12-31 18:45:58 -0800

    Finished ChristmasWithTypes assignment

--- Day.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChristmasWithTypes
-{
-    public partial class Christmas
-    {
-        //TODO Make the property, "Day", type enum
-        public enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday/*Fill out the days of the week*/ };
-    }
-}
